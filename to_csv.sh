97f3bc14ba7c61549b811b04fec0b4c1a2db69a3
Kahvi Patel <iamkahvi@gmail.com>
2021-08-03 22:09:40 -0700

    Updating Readme and cleaning up

--- to_csv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-gawk '
-    BEGIN { print "movieId,customerId,rating,date" }
-    { n=match($0, /^(.*):/, arr); 
-    if (n==1)
-        movie=arr[1]
-    else
-        print movie","$0 }' $1 > $1.csv
