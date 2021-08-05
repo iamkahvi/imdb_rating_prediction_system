190df9f20f60c420e517337d79b52254905c8614
Nolan <kurylon@telus.net>
2021-08-04 21:56:08 -0700

    some new code

--- simpleNN.py
+++ simpleNN.py
@@ -1,8 +1,5 @@
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.metrics import mean_absolute_error
-from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import keras
@@ -16,41 +13,31 @@ X = df.drop(
     ["imdb_title_id", "title", "weighted_average_vote"],
     axis=1,
 ).astype(float)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7)
-
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-
-
-
 
 
 num_dimensions = X.shape[1]
 
-# define the keras model
+# define the keras NN model
+kf = KFold(n_splits=5, random_state=True, shuffle=True)
+for train_index, test_index in kf.split(X):
+    
+    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 
-model = keras.Sequential([
-    keras.layers.Dense(num_dimensions * 2, input_dim=num_dimensions, activation="relu", kernel_initializer="he_normal"),
-    keras.layers.Dense(128, activation='sigmoid'), # hidden layer
-    keras.layers.Dense(1, activation="linear") # output layer
-])
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
 
-model.compile(loss="mse", optimizer="adam", metrics=["mae"])
+    model = keras.Sequential([
+        keras.layers.Dense(num_dimensions, input_dim=num_dimensions, activation="relu", kernel_initializer="normal"), #hidden
+        keras.layers.Dense(1, activation="linear") # output layer
+    ])
 
-# model = Sequential()
-# model.add(
-#     Dense(num_dimensions * 2, input_dim=num_dimensions, activation="relu", kernel_initializer="he_normal")
-# )
-# model.add(Dense(num_dimensions, activation="sigmoid", kernel_initializer="he_normal"))
-# model.add(Dense(10, activation="relu", kernel_initializer="he_normal"))
-# model.add(Dense(1, activation="linear"))
+    model.compile(loss="mse", optimizer="adam", metrics=["mae"])
 
-
-
-model.fit(X_train, y_train, epochs=10, batch_size=10)
+    model.fit(X_train, y_train, epochs=10, batch_size=10)
 
 
-yhat = model.predict(X_test)
-error = mean_absolute_error(y_test, yhat)
-print("MAE: %.3f" % error)
+    yhat = model.predict(X_test)
+    error = mean_absolute_error(y_test, yhat)
+    print("MAE: %.3f" % error)
